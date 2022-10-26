f053abaf14b242a30c5d71577b4e0a3f21ccb045
Debriks <deborah.robbiano@gmail.com>
2022-10-26 19:09:43 +0200

    JS

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
 const btnHamburger = document.getElementById("btnHamburger");
+const body = document.querySelector("body");
 const menuMobile = document.getElementById("menuMobile");
 const overlay = document.getElementById("overlay");
 
 btnHamburger.addEventListener("click", () => {
-  btnHamburger.classList.toggle("close");
-  overlay.classList.toggle("show");
-  menuMobile.classList.toggle("show");
+  if (!btnHamburger.classList.contains("open")) {
+    btnHamburger.classList.add("open");
+    menuMobile.classList.add("show");
+    overlay.classList.add("show");
+    body.classList.add("noscroll");
+  } else {
+    btnHamburger.classList.remove("open");
+    menuMobile.classList.remove("show");
+    overlay.classList.remove("show");
+    body.classList.remove("noscroll");
+  }
 });
